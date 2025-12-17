da88bb4b19957e8517f27c809b35d716858d65b7
kacper <kacper.popowicz@wp.pl>
2025-12-17 20:29:15 +0100

    1.4

--- Frontend/src/pages/Glowna.jsx
+++ Frontend/src/pages/Glowna.jsx
@@ -1,22 +1,35 @@
 import baner from './media/baner.jpg'
-
-
+import React from 'react'
+import './CSS/Glowna.css'
 
 export default function Glowna() {
 return (
-
-
-
-    <div>
-
+<div>
+    <div id ="baner">
     <img src={baner} alt="baner" />
     </div>
+
+    <div id='bloki'>
+        <p>Regularne badania kontrolne ratują życie 🩺</p>
+        <p>Seniorzy 80+ są szczególnie narażeni na zawał ❤️</p>
+        <p>Dbaj o swoje zdrowie - umawiaj wizyty online! 💻</p>
+        <p>Szczepienia chronią przed groźnymi chorobami 💉</p>
+        <p>Mierz ciśnienie – nadciśnienie często nie daje objawów 📊</p>
+        
+    </div>
     
-    
 
 
 
 
 
+
+
+
+
+
+
+
+</div>
 )
 }
