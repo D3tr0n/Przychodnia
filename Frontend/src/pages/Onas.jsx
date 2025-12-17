8b01ae518a08e1c9c1fe12b6169866f120cf6077
Kacper <kacperlazorczyk@gmail.com>
2025-12-17 23:45:02 +0100

    2.7

--- Frontend/src/pages/Onas.jsx
+++ Frontend/src/pages/Onas.jsx
@@ -2,6 +2,12 @@ import probki from './media/onasprobki.jpg'
 import kardiologialogo from './media/kardiologialogo.png'
 import neurologialogo from './media/neurologialogo.png'
 import urologialogo from './media/urologialogo.png'
+import morfologialogo from './media/morfologialogo.png'
+import radiologialogo from './media/radiologialogo.png'
+import pulmonologialogo from './media/pulmonologialogo.png'
+import doktorlogo from './media/doktorlogo.png'
+import diagnozalogo from './media/diagnozalogo.png'
+import leczenielogo from './media/leczenielogo.png'
 import './CSS/Onas.css'
 
 
@@ -70,6 +76,46 @@ export default function Onas(){
 
                     <div class="uslugi-element">
 
+                        <img src={morfologialogo} alt="Morfologia Logo"/>
+
+                        <h2>Morfologia</h2>
+
+                        <h3>
+                            Wykonujemy podstawowe badania krwi,
+                            które pomagają szybko ocenić stan Twojego zdrowia
+                            i wcześnie wykryć ewentualne nieprawidłowości.
+                        </h3>
+
+                    </div>
+
+                    <div class="uslugi-element">
+
+                        <img src={radiologialogo} alt="Radiologia Logo"/>
+
+                        <h2>Radiologia</h2>
+
+                        <h3>
+                            Zapewniamy nowoczesną diagnostykę obrazową,
+                            dzięki której lekarz może postawić trafną diagnozę i zaplanować skuteczne leczenie.
+                        </h3>
+
+                    </div>
+
+                    <div class="uslugi-element">
+
+                        <img src={pulmonologialogo} alt="Pulmonologia Logo"/>
+
+                        <h2>Pulmonologia</h2>
+
+                        <h3>
+                            Pomagamy zadbać o zdrowe oddychanie, diagnozując i lecząc choroby płuc oraz dróg oddechowych,
+                            abyś mógł swobodnie oddychać i czuć się lepiej każdego dnia.
+                        </h3>
+
+                    </div>
+
+                    <div class="uslugi-element">
+
                         <img src={urologialogo} alt="Urologia Logo"/>
 
                         <h2>Urologia</h2>
@@ -79,6 +125,85 @@ export default function Onas(){
                         </h3>
 
                     </div>
+                    
+
+                </div>
+
+            </div>
+
+            <div id="statystyki">
+
+                <div class="s-element">
+
+                    <h2>35+</h2>
+                    <h4>Międzynarodowych nagród</h4>
+
+                </div>
+
+
+                <div class="s-element">
+
+                    <h2>125+</h2>
+                    <h4>Wyspecjalizowanych doktorów</h4>
+
+                </div>
+
+                <div class="s-element">
+
+                    <h2>5k+</h2>
+                    <h4>Zadowolonych pacjentów</h4>
+
+                </div>
+
+                <div class="s-element">
+
+                    <h2>8k+</h2>
+                    <h4>Udanych operacji</h4>
+
+                </div>
+
+            </div>
+
+            <div id="dzialanie">
+
+                <h1>Sprawdź jak działamy</h1>
+
+                <div id="dzielementy">
+
+                    <div class="dzelement">
+
+                    <img src={doktorlogo} alt="Doktor Logo" />
+
+                    <h2>Rozmowa z pacjentem</h2>
+
+                    <h3>Zaczynamy od uważnego wysłuchania Twoich potrzeb i obaw,
+                        aby lepiej zrozumieć problem i zapewnić Ci poczucie bezpieczeństwa.
+                    </h3 >
+
+                </div>
+
+                <div class="dzelement">
+
+                    <img src={diagnozalogo} alt="Diagnoza Logo" />
+
+                    <h2>Diagnoza</h2>
+
+                    <h3>Na podstawie rozmowy i badań dokładnie określamy przyczynę dolegliwości,
+                        aby zaproponować najlepsze rozwiązanie.
+                    </h3 >
+
+                </div>
+
+                <div class="dzelement">
+
+                    <img src={leczenielogo} alt="Leczenie Logo" />
+
+                    <h2>Leczenie</h2>
+
+                    <h3>Dobieramy indywidualny plan leczenia, 
+                    skupiając się na skuteczności, komforcie i szybkim powrocie do zdrowia.</h3 >
+
+                </div>
 
                 </div>
 
