a78352ffb47dee2279a07287c3eebe9d0364ebe0
kacper <kacper.popowicz@wp.pl>
2026-01-13 19:26:44 +0100

    3.4

--- Frontend/src/pages/Kontakt.jsx
+++ Frontend/src/pages/Kontakt.jsx
@@ -6,7 +6,6 @@ export default function Kontakt() {
     <div >
       <div id='kontakt'>
       <div id='kontakt2'>
-        <h2>Kontakt</h2>
 
         <p><strong>Przychodnia ProHealth</strong></p>
         <p>ul. Zdrowa 12</p>
@@ -17,8 +16,7 @@ export default function Kontakt() {
       </div>
 
     <div id='godziny'>
-        <p>🕒 Godziny otwarcia:</p>
-
+        <p>🕒 <strong>Godziny otwarcia:</strong></p>
           <p>Poniedziałek - Piątek: 8:00 - 18:00</p>
           <p>Sobota: 9:00 - 13:00</p>
           <p>Niedziela: nieczynne</p>
